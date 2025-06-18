c19ffbe9773292793376851207ebd87dd3b882b1
JhonElton <eltonjhonbantolino@gmail.com>
2025-06-18 15:31:56 +0800

    Comite UserProfiling

--- Activity1/Program.cs
+++ Activity1/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace UserProfiling
+{
+    class Profile
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+        public string Location { get; set; }
+        public string Occupation { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call the function to collect profile information
+            Profile userProfile = CollectProfile();
+
+            Console.Write("Thank you for taking the form!");
+
+            Console.WriteLine(" Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        // Profiling Functions
+        static Profile CollectProfile()
+        {
+            Profile profile = new Profile();
+
+            Console.Write("Enter your name: ");
+            profile.Name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            profile.Age = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter you gender: ");
+            profile.Gender = Console.ReadLine();
+
+            Console.Write("Please enter your Contact Number: ");
+            profile.ContactNumber = Console.ReadLine();
+
+            Console.Write("Please enter your Email: ");
+            profile.Email = Console.ReadLine();
+
+            Console.Write("Please enter your Location: ");
+            profile.Location = Console.ReadLine();
+
+            Console.Write("Enter your occupation: ");
+            profile.Occupation = Console.ReadLine();
+
+            return profile;
+        }
+    }
+}
